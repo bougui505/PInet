9cab1cc05aeff37b655e51aaf7980e5e9bf5f1b9
Vincentx15 <vincent.mallet96@gmail.com>
2021-06-03 14:13:28 +0200

    added prediction script

--- utils/SimplePredictor.py
+++ utils/SimplePredictor.py
@@ -12,64 +12,44 @@ import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
 
+random.seed(random.randint(1, 10000))
+torch.manual_seed(random.randint(1, 10000))
 
-random.seed(random.randint(1, 10000) )
-torch.manual_seed(random.randint(1, 10000) )
+def get_classifier(weights_path = '../models/dbd_aug.pth', device='cpu'):
+    num_classes = 2
+    classifier = PointNetDenseCls12(k=num_classes, feature_transform=False, pdrop=0.0, id=5)
+    classifier.to(device)
+    classifier.load_state_dict(torch.load(weights_path))
+    classifier.eval()
 
-filel=sys.argv[1]
-filer=sys.argv[2]
+def get_input(pts_file, device='cpu'):
+    points = np.loadtxt(pts_file).astype(np.float32)
+    coordset = points[:, 0:3]
+    featset = points[:, 3:]
+    featset = featset / np.sqrt(np.max(featset ** 2, axis=0))
+    coordset = coordset - np.expand_dims(np.mean(coordset, axis=0), 0)  # center
+    points[:, 0:5] = np.concatenate((coordset, featset), axis=1)
+    points = torch.from_numpy(points).unsqueeze(0)
 
-
-num_classes = 2
-classifier = PointNetDenseCls12(k=num_classes, feature_transform=False,pdrop=0.0,id=5)
-
-classifier.cuda()
-
-
-PATH='../models/dbd_aug.pth'
-classifier.load_state_dict(torch.load(PATH))
-classifier.eval()
-
-pointsr=np.loadtxt(filer).astype(np.float32)
-pointsl=np.loadtxt(filel).astype(np.float32)
-
-coordsetr = pointsr[:, 0:3]
-featsetr = pointsr[:, 3:]
-
-coordsetl = pointsl[:, 0:3]
-featsetl = pointsl[:, 3:]
-
-featsetr = featsetr / np.sqrt(np.max(featsetr ** 2, axis=0))
-featsetl = featsetl / np.sqrt(np.max(featsetl ** 2, axis=0))
-        
-coordsetr = coordsetr - np.expand_dims(np.mean(coordsetr, axis=0), 0)  # center
-coordsetl = coordsetl - np.expand_dims(np.mean(coordsetl, axis=0), 0)  # center
-
-pointsr[:, 0:5] = np.concatenate((coordsetr, featsetr), axis=1)
-pointsl[:, 0:5] = np.concatenate((coordsetl, featsetl), axis=1)
-
-pointsr=torch.from_numpy(pointsr).unsqueeze(0)
-pointsl=torch.from_numpy(pointsl).unsqueeze(0)
+    memlim = 120000
+    if points.size()[1] + points.size()[1] > memlim:
+        subset_size = points.size()[1] * memlim / (points.size()[1] + points.size()[1])
+        subset = np.random.choice(points.size()[1], subset_size, replace=False)
+        points = points[:, subset, :]
+    points = points.transpose(2, 1)
+    points = points.to(device)
+    return points
 
 
-memlim=120000
-if pointsl.size()[1] + pointsr.size()[1] > memlim:
-    lr = pointsl.size()[1] * memlim / (pointsl.size()[1] + pointsr.size()[1])
-    rr = pointsr.size()[1] * memlim / (pointsl.size()[1] + pointsr.size()[1])
-    ls = np.random.choice(pointsl.size()[1], lr, replace=False)
-    rs = np.random.choice(pointsr.size()[1], rr, replace=False)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+pts_r_file = '../data/2I25/2I25-r.pts'
+pts_l_file = '../data/2I25/2I25-l.pts'
+outfile = '../data/2I25/2I25_prob_r_l.seg'
 
-    pointsr = pointsr[:, rs, :]
-    pointsl = pointsl[:, ls, :]
-    
-pointsr = pointsr.transpose(2, 1).cuda()
-pointsl = pointsl.transpose(2, 1).cuda()
+classifier = get_classifier(device=device)
+points_l = get_input(pts_file=pts_l_file,device=device)
+points_r = get_input(pts_file=pts_r_file,device=device)
 
-classifier = classifier.eval()
-
-pred, _, _ = classifier(pointsr,pointsl)
-
+pred, _, _ = classifier(points_l, points_r)
 pred = pred.view(-1, 1)
-
-np.savetxt(filel[0:4]+'_prob_r_l.seg',torch.sigmoid(pred).view(1, -1).data.cpu())
-    
+np.savetxt(outfile, torch.sigmoid(pred).view(1, -1).data.cpu())
